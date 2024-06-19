9cfe6dba0a9879c496a9ff86b23ac8959b7be7a0
Lan Lan <you@example.com>
2024-06-19 11:10:32 +0700

    lan-commit19-06

---
+++ src/main/java/com/example/woori_base/WooriBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.woori_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriBaseApplication.class, args);
+	}
+
+}
